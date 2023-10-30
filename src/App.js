9b047ece20a7d66105fdc23a95e56ade787362c2
Nitotony <nitotony47@gmail.com>
2023-10-30 14:52:35 +0530

    newest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
-import "./footer";
-import "./title";
-import "./file";
-import Footer from "./footer";
-import Title from "./title";
-import File from "./file";
+
 
 function App() {
   return (
     <div className="App">
-      <Title />
+      <div className="common_title">
+        <div className="app_title">
+          <p>Legalpulse</p>
+        </div>
+        <div className="app_icon">
+          <img src="./dog.jpeg" alt="" />
+        </div>
+      </div>
       <div className="kappa">
         <h1 style={{ color: "black", textAlign: "left" }}>
           Do you have certifications?
@@ -19,9 +21,22 @@ function App() {
           what you can do.
         </h2>
       </div>
+      <div>
+        <div className="alpha">
+          <button className="fileinput">+</button>
+          <h2 className="boom">Your certifications</h2>
+        </div>
+        <div className="beta">
+          <input type="checkbox" name="nofile" className="inp" />
+          <p>Nothing to add? Check the box and keep going</p>
+        </div>
+      </div>
+      <div className="foot">
+        <button className="b1"> back</button>
+        <button className="b2"> Next, add languages</button>
+      </div>
 
-      <File />
-      <Footer />
+      
     </div>
   );
 }
--- src/footer.js
+++
@@ -1,9 +0,0 @@
-import "./footer.css";
-export default function Footer() {
-  return (
-    <div className="foot">
-      <button className="b1"> back</button>
-      <button className="b2"> Next, add languages</button>
-    </div>
-  );
-}
