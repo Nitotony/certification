9e29a4169b426b285f5bd258eade8799033716a9
Nitotony <nitotony47@gmail.com>
2023-10-30 14:31:12 +0530

    new

--- src/file.js
+++ src/file.js
@@ -7,12 +7,10 @@ export default function File() {
         <button className="fileinput">+</button>
         <h2 className="boom">Your certifications</h2>
       </div>
-      <div className="beta">
-        <input type="checkbox" name="nofile" className="inp" />
-        <label htmlFor="nofile">
-          Nothing to add? Check the box and keep going
-        </label>
-      </div>
+        <div className="beta">
+          <input type="checkbox" name="nofile" className="inp" />
+          <p>Nothing to add? Check the box and keep going</p>
+        </div>
     </div>
   );
 }
